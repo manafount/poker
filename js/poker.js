99afc336baa26f31a1ba9d3a639c354869b1cc13
Matt Kerchner <manafount@gmail.com>
2017-01-24 17:19:02 -0800

    fixed tween

--- js/poker.js
+++ js/poker.js
@@ -1,4 +1,6 @@
 import createjs from 'createjs-easeljs';
+import { Tween, Ease } from 'createjs-tweenjs';
+
 import Card from './card';
 import Deck from './deck';
 
@@ -15,6 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log(deck);
 
 
+  createjs.Tween.get(cardImage, { loop: true })
+  .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+  .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+  .to({ alpha: 0, y: 225 }, 100)
+  .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+  .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+
+  createjs.Ticker.setFPS(60);
   createjs.Ticker.addEventListener("tick", handleTick);
   function handleTick(event) {
     stage.update();
